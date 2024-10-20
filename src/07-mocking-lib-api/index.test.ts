cf2a1df1c2f4012363e1e39a3919191f31fd6096
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-10-20 14:12:36 +0300

    feat: add test mocking-lib

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,62 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => {
+  return {
+    throttle: (fn: () => unknown) => fn,
+  };
+});
+const URL = '/todos/1';
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const MOCK_RESOLVED = {
+  userId: 1,
+  id: 1,
+  title: 'delectus aut autem',
+  completed: false,
+};
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mock = jest.fn().mockReturnValue({
+      get: jest.fn().mockResolvedValue({
+        data: {
+          userId: 1,
+          id: 1,
+          title: 'delectus aut autem',
+          completed: false,
+        },
+      }),
+    });
+
+    (axios.create as jest.Mock).mockImplementation(mock);
+    await throttledGetDataFromApi(URL);
+    expect(mock).toHaveBeenCalledWith({
+      baseURL: BASE_URL,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue(MOCK_RESOLVED);
+    const mockCreate = jest.fn().mockReturnValue({
+      get: mockGet,
+    });
+
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+    await throttledGetDataFromApi(URL);
+    expect(mockGet).toHaveBeenCalledWith(URL);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({
+      data: MOCK_RESOLVED,
+    });
+    const mockCreate = jest.fn().mockReturnValue({
+      get: mockGet,
+    });
+
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+    const res = await throttledGetDataFromApi(URL);
+    expect(res).toEqual(MOCK_RESOLVED);
   });
 });
